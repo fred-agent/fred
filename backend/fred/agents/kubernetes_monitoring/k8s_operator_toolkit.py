f2fc8aca426274d504895fd528c0d2b090ee80e7
Simon Cariou <simon.cariou@thalesgroup.com>
2025-05-22 03:47:19 +0000

    feat: add mcp server support in agent configuration

--- backend/fred/agents/kubernetes_monitoring/k8s_operator_toolkit.py
+++ backend/fred/agents/kubernetes_monitoring/k8s_operator_toolkit.py
@@ -2,17 +2,18 @@ from typing import override, List
 
 from langchain_core.tools import BaseToolkit, BaseTool
 from pydantic import Field
+from langchain_mcp_adapters.client import MultiServerMCPClient
 
 class K8SOperatorToolkit(BaseToolkit):
     """
-    Toolkit for K8S monitoring and operator tools
+    Toolkit for K8S operator tools
     """
 
     tools: List[BaseTool] = Field(default_factory=list, description="List of the tools.")
 
-    def __init__(self):
+    def __init__(self, mcp_client: MultiServerMCPClient):
         super().__init__()
-        self.tools = []
+        self.tools = mcp_client.get_tools()
 
     @override
     def get_tools(self) -> list[BaseTool]:
